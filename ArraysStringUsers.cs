d975ab2e9dcaa4bb5347785dd07dc5f7d017a049
OgbekileElizabeth <Ogbekileelizabeth5@gmail.com>
2026-07-05 18:36:44 +0100

    Add project files.

---
+++ ArraysStringUsers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ArraysStringUser
+{
+    internal class ArraysStringUsers
+    {
+        public static string ConcatenateStrings_Two(string first, string second)
+    {
+            return first + second;    
+    }
+    public static string ConcatenateStrings_two(string first, string second)
+        {
+            return first + second;
+        }
+        public static void SubstringOperation()
+        {
+            string f = "ScreenRecord";
+            int startIndex = 6;
+            int length = 6;
+            Console.WriteLine(f.Substring(startIndex,length));
+           
+            string s = "Bluetooth";
+            int startIndex2 = 0;
+            int length2 = 4;
+            Console.WriteLine(s.Substring(startIndex2, length2));
+        }
+
+    
+        public static void ReplaceString()
+        {
+            string str = "Blue Tooth";
+            string replaced = str.Replace("Tooth", "Sea");
+            Console.WriteLine(replaced);
+        }
+
+        public static void SearchSubstring()
+        {
+            string str = "Blue Tooth";
+            int index = str.IndexOf("Tooth");
+
+            Console.WriteLine($"Index of 'Tooth': {index}");
+
+            bool containsooth = str.Contains("ooth");
+            Console.WriteLine($"Contains 'ooth': {containsooth}");
+
+            bool containsSea = str.Contains("Sea");
+            Console.WriteLine($"Contains 'Sea': {containsSea}");
+        }
+        public static void ComparisonofString()
+        {
+            string s1 = "Blue", s2 = "Blue";
+            bool areEqualOne = s1 == s2; // Case-sensitive comparison
+            bool areEqualTwo = s1.Equals(s2, StringComparison.OrdinalIgnoreCase);
+
+            Console.WriteLine($"Case-sensitive comparison: {areEqualOne}");
+            Console.WriteLine($"Case-sensitive comparison: {areEqualTwo}");
+
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArraysStringUser
+{
+    internal class Program
+    {
+     static void Main(string[] args)
+    {
+                 
+            string s1 = "Screen";
+            string s2 = "Record";
+
+            var resultThree = ArraysStringUsers.ConcatenateStrings_two(s1, s2);
+            Console.WriteLine(resultThree);
+            
+            string Ap1 = "Blue";
+            string Ap2 = "Tooth";
+
+            var resultThreeKpi = ArraysStringUsers.ConcatenateStrings_two( Ap1, Ap2);
+            Console.WriteLine(resultThreeKpi);
+
+            ArraysStringUsers.SubstringOperation();
+            ArraysStringUsers.SearchSubstring();
+            ArraysStringUsers.ReplaceString();
+            ArraysStringUsers.ComparisonofString();
+        }
+        
+        }
+    }
+    
+
+       
+   
+
